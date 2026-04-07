00cfd60109219e534c6d96762b2c0ae7740762e8
Anirban Das <anirbandas6778788@gmail.com>
2026-04-07 15:31:18 +0530

    fix: change publicDir to prevent build collision with old site files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Gkk-Web/', // Updating to the new repository name
-  publicDir: 'Gkk-Web',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
